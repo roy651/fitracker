b8e92829a41a8f8d02518456f225a0d0540a2684
ROY ABITBOL <Roy.Abitbol@il.ibm.com>
2026-01-04 21:53:13 +0200

    chore: Update index.html precache revision.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.oucjtnnub08"
+    "revision": "0.c9dc3ko1344"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
